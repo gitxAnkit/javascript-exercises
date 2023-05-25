e26df463b39abf52ae55481c70e1e573c20be5bb
Ankit Verma <ankitv9936@gmail.com>
2023-05-25 23:21:27 +0530

    Practice TDD exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let str = string;
+    if( num == 0){
+        str ='';
+    }
+    else if( num < 0){
+        str ='ERROR';
+    }
+    else{
+        for (let i=1; i<num; i++){
+        
+            str =str + string;
+            
+        }
+    }
+    return str;
 };
 
 // Do not edit below this line
