4f75353730b884f59f4ec5d3e5cf18a9f30120cf
Ankit Verma <ankitv9936@gmail.com>
2023-05-25 23:39:57 +0530

    Practice TDD exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(words) {
 
+    let string = words;
+    let length = string.length;
+    let reverse ='';
+
+    for (let i=length-1; i>=0; --i){
+        reverse = reverse + string.charAt(i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
