aaf7692575fadca1e3827e697feb7f2a5b557521
Ankit Verma <ankitv9936@gmail.com>
2023-05-26 11:28:45 +0530

    Practice TDD exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
+    args.forEach((arg) => {
+        index = array.indexOf(arg);
+        if(index >-1){
+            array.splice(index,1);
+        }       
+    });
+
+    return array;
 };
-
+// console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
