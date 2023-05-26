916ffc77d218ae9bc3013fc8ee08024fad2c1814
Ankit Verma <ankitv9936@gmail.com>
2023-05-26 12:10:45 +0530

    Practice TDD exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  
+        if( !Number.isInteger(start) || !Number.isInteger(end) ){
+            return "ERROR";
+        }
+        let sum =0;
+        if( start >0 && end >0){
+             for (let i=Math.max(start,end); i>0; --i){
+                 sum = sum +i;
+             } 
+             return sum;
+        }
+        
+         else {
+            return 'ERROR'
+         }
+    
 };
-
+console.log(sumAll(1,4));
 // Do not edit below this line
 module.exports = sumAll;
