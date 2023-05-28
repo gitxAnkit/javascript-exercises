9487dbabf3f07734f82f23e33ed1998b63ce05bd
Ankit Verma <ankitv9936@gmail.com>
2023-05-28 09:46:29 +0530

    Practice TDD exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+        let inCelsius = (temp - 32) * 5/9;
+        return Number(inCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+        let inFahrenheit = temp*9/5 + 32;
+        return Number(inFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
