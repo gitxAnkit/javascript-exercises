aafcc69c5e6e52065c23c8b481ab97b3a25274ca
Ankit Verma <ankitv9936@gmail.com>
2023-05-28 11:24:25 +0530

    Practice TDD exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return Number(num1 +num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+  return Number(num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+    let sum=0;
+    for(number of numbers){
+      sum =sum +number;
+    }
+    return Number(sum);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+    let product =1;
+    for(number of numbers){
+      product = product*number;
+    }
+    return Number(product);
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return Number(Math.pow(num1,num2));
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let fact=1;
+  if (number >1){
+    for (let i =2; i<=number; ++i){
+      fact= fact*i;
+    }
+  } 
+  return Number(fact);
 };
 
 // Do not edit below this line
