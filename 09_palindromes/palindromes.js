4588b2924f767163c5a57aa23a7bfd61daf61fe7
Ankit Verma <ankitv9936@gmail.com>
2023-05-28 20:44:45 +0530

    Practice TDD exercise 09 and learned about regexp

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    
+    const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return processedString.split("").reverse().join("") == processedString;
+    
 };
-
+console.log(palindromes(''));
 // Do not edit below this line
 module.exports = palindromes;
