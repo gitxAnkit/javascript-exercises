6bee16167fc5be4310bc2a4899cac4cd415f0e39
Ankit Verma <ankitv9936@gmail.com>
2023-06-06 16:49:11 +0530

    Practice TDD exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num <0) return 'OOPS';
+    if (num ===0) return 0;
+    let first=0;
+    let second =1;
+    for(let i=1;i<num; i++){
+        const next = second;
+        second = first + second;
+        first =next;
+      }
+      return second;
 };
-
+console.log(fibonacci(15));
 // Do not edit below this line
 module.exports = fibonacci;
